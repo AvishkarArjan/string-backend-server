df1cd0564f7243046a78431ad695807cbcce7664
Avishkar <avishkar2108@gmail.com>
2023-03-06 21:00:29 +0530

    set cookie header - SameSite=None # part2

--- router/auth.js
+++ router/auth.js
@@ -179,7 +179,7 @@ router.post("/admin/employees", authenticate, async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    res.setHeader('Set-Cookie', 'isLoggedin=true', 'SameSite=None');
+    res.setHeader('Set-Cookie', 'isLoggedin=true, SameSite=None');
     let token;
     const { email, password } = req.body;
     
